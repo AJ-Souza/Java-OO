967189c086513b899ba6e9fae8df6bc96a172e8d
asjsouzajr <asjsouzajr@hotmail.com>
2022-09-20 20:13:37 +0200

    implementando o paradigma do Encapsulamento atraves de Getters e Setters

--- src/Conta.java
+++ src/Conta.java
@@ -1,8 +1,36 @@
 public class Conta {
-	double saldo;
-	int agencia;
-	int numero;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
 	Cliente titular;
+
+	public double getSaldo() {
+		return saldo;
+	}
 
 	public void deposita(double valor) {
 		this.saldo += valor;
--- src/Teste.java
+++ src/Teste.java
@@ -5,26 +5,30 @@ public class Teste {
 		
 		contaDoPaulo.titular = new Cliente();
 
-		contaDoPaulo.titular.nome = "Paulo Silveira";
-		contaDoPaulo.titular.cpf = "222.222.222-22";
-		contaDoPaulo.titular.profissao = "Desenvolvedoro";
+		contaDoPaulo.titular.setNome("Paulo Silveira");
+		contaDoPaulo.titular.setCpf("222.222.222-22");
+		contaDoPaulo.titular.setProfissao("Desenvolvedoro");
+		contaDoPaulo.setAgencia(123);
+		contaDoPaulo.setNumero(789987);
 		
 		
 		contaDaMarcela.titular = new Cliente();
 
-		contaDaMarcela.titular.nome = "Marcela";
-		contaDaMarcela.titular.cpf = "111.111.111-11";
-		contaDaMarcela.titular.profissao = "Desenvolvedora";
+		contaDaMarcela.titular.setNome("Marcela");
+		contaDaMarcela.titular.setCpf("111.111.111-11");
+		contaDaMarcela.titular.setProfissao("Desenvolvedora");
+		contaDaMarcela.setAgencia(456);
+		contaDaMarcela.setNumero(963369);
 		contaDaMarcela.deposita(100.0);
 		contaDaMarcela.saca(20);
 		contaDaMarcela.transfere(20, contaDoPaulo);
 		
 		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("Titular: " + contaDaMarcela.titular.nome + ", Saldo: "
-				+ contaDaMarcela.saldo);
+		System.out.println("Titular: " + contaDaMarcela.titular.getNome() + ", Saldo: "
+				+ contaDaMarcela.getSaldo());
 		
 		System.out.println("-------------------------------");
-		System.out.println("Titular: " + contaDoPaulo.titular.nome + ", Saldo: "
-				+ contaDoPaulo.saldo);
+		System.out.println("Titular: " + contaDoPaulo.titular.getNome() + ", Saldo: "
+				+ contaDoPaulo.getSaldo());
 	}
 }
